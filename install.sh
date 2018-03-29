3a52ca6692eda4e8128dd05d69ed0cc3aa8122ea
Franklin <franklin@flatiron.com>
2018-03-28 23:55:51 -0400

    updating install script to install homebrew, python, pip

--- install.sh
+++ install.sh
@@ -1,3 +1,15 @@
+#!/bin/bash -e
+
+# install homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# install python
+brew install python
+
+# install pip
+sudo easy_install pip
+
+# install python packages
 pip install pyzipcode
 # geopy docs: http://geopy.readthedocs.io/en/latest/
 pip install geopy
