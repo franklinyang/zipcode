3e8ac6bc5f1e18e1c7d95431b44e2db7cbe83eae
Franklin <franklin@flatiron.com>
2018-03-28 23:46:52 -0400

    Parameterizing run_me script

--- run_me.py
+++ run_me.py
@@ -1,7 +1,12 @@
+import sys
+
+import zipcode
+
+
 if __name__ == '__main__':
-    import zipcode
-    zipcode_1 = '02142'
-    zipcode_2 = '78572'
+    assert len(sys.argv) == 3, 'Please specify two zipcodes to calculate distance between'
+    zipcode_1 = sys.argv[1]
+    zipcode_2 = sys.argv[2]
     latlong_cambridge = zipcode.get_lat_and_long(zipcode_1) 
     latlong_texas = zipcode.get_lat_and_long(zipcode_2) 
     print 'Distance between {zipcode_1} and {zipcode_2} is {distance} miles'.format(
